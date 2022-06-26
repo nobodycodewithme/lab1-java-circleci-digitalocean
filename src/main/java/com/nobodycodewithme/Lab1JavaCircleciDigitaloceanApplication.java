cc83b5413ce6732a2fd111eee04bcd860fbecd10
Mai Kien Cuong <maikiencuongiuh@gmail.com>
2022-06-27 01:53:44 +0700

    fix open api

--- src/main/java/com/nobodycodewithme/Lab1JavaCircleciDigitaloceanApplication.java
+++ src/main/java/com/nobodycodewithme/Lab1JavaCircleciDigitaloceanApplication.java
@@ -2,12 +2,17 @@ package com.nobodycodewithme;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @OpenAPIDefinition(
         info = @Info(title = "Quan ly sinh vien", version = "1.0", description = "Documentation Quan ly sinh vien API v1.0"))
@@ -25,5 +30,14 @@ public class Lab1JavaCircleciDigitaloceanApplication {
                 .setMatchingStrategy(MatchingStrategies.STANDARD);
 
         return modelMapper;
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        List<String> servers = List.of("https://api-quanlysinhvien.ddns.net/",
+                "http://localhost:8080");
+        return new OpenAPI().servers(
+                servers.stream().map(serverUrl -> new Server().url(serverUrl)).collect(Collectors.toList())
+        );
     }
 }
